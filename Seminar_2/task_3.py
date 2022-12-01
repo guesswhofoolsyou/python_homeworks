90bafd92e0980fb3f3e1c0febb7c6e848197e744
Egor <egs046@gmail.com>
2022-12-02 02:51:37 +0300

    fixed 2 task

--- Seminar_2/task_3.py
+++ Seminar_2/task_3.py
@@ -5,26 +5,24 @@
 from random import randint
 
 def fill_random_array(num):
-    result_array = [randint(-10,11) for x in range(num)]
+    result_array = [randint(-10,10) for x in range(num)]
     return result_array
+
 
 def fill_zero (array, num):
     result_array = []
     array_length = num
-    counter = 0
-    for i in range (0, array_length):
-        if array[counter] < 0:
-            result_array[i] = array[counter]
-            result_array[i+1] = 0
-            array_length += 1
-            counter += 1
-            i += 1
-        elif array[counter] >= 0:
-            result_array[i] = array[counter]
-    print (result_array)
-
+    for i in range (0, array_length+1):
+        if array[i] < 0:
+            if i == len(array):
+                array.append(0)
+            else:
+                array.insert(i+1, 0)    
+        else:
+            continue
+    print (array)
 
 num = int(input("Введите длину массива:\n"))
 array = fill_random_array(num)
 print(array)
-result_array = fill_zero(array, num)
+fill_zero(array, num)
